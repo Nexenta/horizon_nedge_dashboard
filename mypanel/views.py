3ea17b8f4044c047da8d95ccc48263c556b286e5
Victor Piousbox <piousbox@gmail.com>
2014-08-05 18:20:52 -0700

    blah

--- mypanel/views.py
+++ mypanel/views.py
@@ -3,14 +3,20 @@ from horizon import views
 
 from openstack_dashboard.dashboards.mydashboard.mypanel import tabs as mydashboard_tabs
 
+import sys
+import pycurl
 
 class IndexView(views.APIView):
     tab_group_class = mydashboard_tabs.MypanelTabs
     template_name = 'mydashboard/mypanel/index.html'
-    clusters = "clusters123"
 
     def get_data(self, request, context, *args, **kwargs):
-        context['clusters'] = 'These are my clusters'
+        
+#        c = pycurl.Curl()
+#        c.setopt(c.URL, 'http://172.31.98.28:8081/')
+#        clusters = c.perform()
+#        context['clusters'] = clusters # 'These are my clusters'
+        context['python_version'] = sys.version
 
         # Add data to the context here...
         return context
